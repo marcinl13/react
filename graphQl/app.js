f31dc85cf882c230909990e78d218ea9c718a7d2
marcinl13 <marcinlpotrykus@gmail.com>
2019-05-07 15:00:26 +0200

    graphqk

---
+++ graphQl/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.use(
+  '/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+          events: [String!]!
+        }
+
+        type RootMutation{
+          createEvent(name:String):String
+        }
+        
+        schema{
+          query:RootQuery
+          mutation:RootMutation
+        }`
+    ),
+    rootValue: {
+      events: () => { return ['asd', 'asdasd'] },
+      createEvent: (args) => {
+        const names = args.name
+        return names
+      }
+    }, graphiql: true
+  })
+)
+
+
+app.listen(5000)
